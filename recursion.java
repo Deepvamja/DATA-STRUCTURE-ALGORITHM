180f8e3c8930d0963c1c4d37112772918c003ad3
deepvamja <143236575+Deepvamja@users.noreply.github.com>
2024-08-21 07:57:43 +0530

    Add files via upload

---
+++ recursion.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class recursion {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int ans = 1;
+
+        for (int i = 1; i <= n; i++) {
+            ans = ans * i;
+        }
+        System.out.println(ans);
+        sc.close();
+    }
+
+}
