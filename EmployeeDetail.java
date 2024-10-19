2d98f338f47ba83ce98c3e91e61c00129c131099
deepvamja <143236575+Deepvamja@users.noreply.github.com>
2024-10-19 11:04:03 +0530

    Add files via upload

---
+++ EmployeeDetail.java
@@ -0,0 +1,13 @@
+
+public class EmployeeDetail {
+
+    public EmployeeDetail(int id, String name, String designation, double salary) {
+        //TODO Auto-generated constructor stub
+    }
+
+    public void displayDetails() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'displayDetails'");
+    }
+
+}
