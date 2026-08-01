82de95a446e91173d77b1f68ea056389b44971ef
deepvamja <143236575+Deepvamja@users.noreply.github.com>
2026-08-01 11:11:26 +0530

    Add files via upload

---
+++ leetcode_392.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+
+        int i = 0; // Pointer for s
+        int j = 0; // Pointer for t
+
+        while (i < s.length() && j < t.length()) {
+
+            if (s.charAt(i) == t.charAt(j)) {
+                i++;
+                j++;
+            } else {
+                j++;
+            }
+        }
+
+        return i == s.length();
+    }
+}
