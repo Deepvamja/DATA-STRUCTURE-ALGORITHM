5cc193a22d757a9867a4078db742cf0f4d15ff3d
deepvamja <143236575+Deepvamja@users.noreply.github.com>
2026-08-03 23:39:33 +0530

    Add files via upload

---
+++ leetcode_TrappingRainWater_42.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int trap(int[] height) {
+
+        if (height == null || height.length == 0) {
+            return 0;
+        }
+
+        int left = 0;
+        int right = height.length - 1;
+
+        int leftMax = height[left];
+        int rightMax = height[right];
+
+        int water = 0;
+
+        while (left < right) {
+
+            if (leftMax < rightMax) {
+
+                left++;
+
+                if (height[left] > leftMax) {
+                    leftMax = height[left];
+                } else {
+                    water += leftMax - height[left];
+                }
+
+            } else {
+
+                right--;
+
+                if (height[right] > rightMax) {
+                    rightMax = height[right];
+                } else {
+                    water += rightMax - height[right];
+                }
+            }
+        }
+
+        return water;
+    }
+}
