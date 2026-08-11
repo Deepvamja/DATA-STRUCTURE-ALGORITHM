0d23b1ee87e1330a1ba316c91b1173db6f22b75e
deepvamja <143236575+Deepvamja@users.noreply.github.com>
2026-08-11 10:56:53 +0530

    Add files via upload

---
+++ gfg_tripletsWithSmallerSum.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+
+class Solution {
+
+    int countTriplets(int sum, int[] arr) {
+
+        Arrays.sort(arr);
+
+        int n = arr.length;
+        int count = 0;
+
+        for (int i = 0; i < n - 2; i++) {
+
+            int left = i + 1;
+            int right = n - 1;
+
+            while (left < right) {
+
+                int currentSum = arr[i] + arr[left] + arr[right];
+
+                if (currentSum < sum) {
+
+                    count += (right - left);
+                    left++;
+
+                } else {
+
+                    right--;
+                }
+            }
+        }
+
+        return count;
+    }
+}
